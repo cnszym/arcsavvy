4942e76768a6442c15e26812bba2b6d800a7ba03
cnszym <cnszym@nibbler>
2015-07-18 12:41:28 +0200

    slice tests

--- test.js
+++ test.js
@@ -4,13 +4,25 @@ var fs = require('fs');
 var _ = require('underscore');
 var nb_states = 8;
 
+if( process.argv[2]=='index1' || process.argv[2]==undefined || process.argv[2]=='all' ) {
+// test indexing exploration functions
+console.log('TEST explore_recursive');
 explore_recursive('tests', function(n,t,f,c) {
   console.log(t,n);
 });
 
 console.log(get_element('./tests/state0/description.txt', 'state0/description.txt'));
-console.log(construct_index(construct_file_index('./tests/state5')));
+}
 
+if( process.argv[2]=='index2' || process.argv[2]==undefined || process.argv[2]=='all' ) {
+// test full indexing function
+console.log('TEST construct_index');
+console.log(construct_index(construct_file_index('./tests/state8')));
+}
+
+if( process.argv[2]=='changes1' || process.argv[2]==undefined || process.argv[2]=='all' ) {
+// test change detection for one file
+console.log('TEST compute_changes (one file)');
 console.log("===== tracking only file2.txt =====")
 for( var i=0; i<nb_states; ++i ) {
   var s = 'tests/state'+i, e = 'tests/state'+(i+1);
@@ -21,7 +33,11 @@ for( var i=0; i<nb_states; ++i ) {
     compute_changes(start, end, end['file2.txt']);
   }
 }
+}
 
+if( process.argv[2]=='changes2' || process.argv[2]==undefined || process.argv[2]=='all' ) {
+// test change detection for all files in offline mode
+console.log('TEST compute_changes (all offline)');
 console.log("===== tracking all files (offline mode) =====")
 for( var i=0; i<nb_states; ++i ) {
   var s = 'tests/state'+i, e = 'tests/state'+(i+1);
@@ -30,7 +46,11 @@ for( var i=0; i<nb_states; ++i ) {
       end = construct_file_index(e,'');
   compare_indexes(start,end);
 }
+}
 
+if( process.argv[2]=='changes3' || process.argv[2]==undefined || process.argv[2]=='all' ) {
+// test change detection for all files in online mode
+console.log('TEST compute_changes (all online)');
 console.log("===== tracking all files (online mode) =====")
 var end_index;
 for( var i=0; i<nb_states; ++i ) {
@@ -40,12 +60,15 @@ for( var i=0; i<nb_states; ++i ) {
   end_index = compare_indexes(start,e);
 }
 console.log(end_index);
+}
 
-// test cycle: archive->check->restore->diff
+if( process.argv[2]=='loop' || process.argv[2]==undefined || process.argv[2]=='all' ) {
+// test cycle: archive->check->restore
 // clean the temp directory
 cp.spawnSync('rm',['-rf','temp']);
 cp.spawnSync('mkdir', ['temp']);
 
+console.log('TEST snapshot_archive/check_archive/restore_archive');
 for( var i=0; i<=nb_states; ++i ) {
   var s = 'tests/state'+i, arc = 'temp/archive', status, status_success;
 
@@ -70,4 +93,5 @@ for( var i=0; i<=nb_states; ++i ) {
   // restore
   status = restore_archive(arc, 'temp/state'+i);
   console.log(status);
+}
 }
